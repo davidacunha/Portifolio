9b13f74223a66f99bdc6cb21377ff4a02e4a1a45
davidacunha <davidacunha26@gmail.com>
2024-10-23 15:09:12 -0300

    Atualizações endereços de instalação de dependencias

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@
 echo "Iniciando servidor da aplicação"
 
 # Navegar até o diretório da aplicação e iniciar o servidor
-cd home/ec2-user/Portifolio/my-app/backend
+cd Portifolio/my-app/backend
 npm install
 node index.js 
